bad5b39ebb3c961b59a89a47a8b2bbac99f4a44e
msjang4 <msjang7115@gmail.com>
2024-06-14 22:17:06 +0900

    ✨Feat: 스터디 리스트 페이지 디자인 추가 및 root layout, style 수정

---
+++ app/study/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import Header from '@/components/header/header';
+import { StudyList } from '@/components/study/studylist';
+
+export default function Signup() {
+  return (
+    <div>
+    <Header />
+  <StudyList />
+
+    </div>
+  );
+}
