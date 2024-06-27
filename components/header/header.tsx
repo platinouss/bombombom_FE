58602eaf5f5c626ccb411f949e517072a0c3ba66
msjang4 <msjang7115@gmail.com>
2024-06-27 12:54:01 +0900

    ✨Fix: Button 컴포넌트 Import 경로 수정 🧹Chore: Vscode에서 저장시 organizeImport액션  추가

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,25 +1,23 @@
 'use client';
 
-import Link from 'next/link';
-import {
-  NavigationMenu,
-  NavigationMenuList,
-  NavigationMenuLink
-} from '@/components/ui/navigation-menu';
-import { Button } from '@/components/ui/button';
 import HeaderNavMenu from '@/components/header/header-nav-menu';
 import UserProfileDropDown from '@/components/header/user-profile-drop-down';
+import { Button } from '@/components/ui/button/button';
+import {
+  NavigationMenu,
+  NavigationMenuList
+} from '@/components/ui/navigation/navigation-menu';
+import Link from 'next/link';
 
-import { useEffect } from 'react';
-import { useRecoilState } from 'recoil';
+import { me } from '@/lib/api/users/me'; // API 함수 경로에 맞게 수정
 import { userState } from '@/recoil/userAtom';
 import { usePathname } from 'next/navigation';
-import { me } from '@/lib/api/users/me'; // API 함수 경로에 맞게 수정
+import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
 
 export default function Header() {
   const [user, setUser] = useRecoilState(userState);
   const pathname = usePathname();
-  
 
   useEffect(() => {
     if (localStorage.getItem('accessToken') === null || user !== null) {
@@ -37,8 +35,7 @@ export default function Header() {
     fetchUser();
   }, [user, setUser]);
 
-  return (
-    pathname === '/login' ? null :
+  return pathname === '/login' ? null : (
     <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
       <Link href="/" className="mr-6 hidden lg:flex" prefetch={false}>
         <MountainIcon className="h-6 w-6" />
@@ -52,9 +49,13 @@ export default function Header() {
         </NavigationMenuList>
       </NavigationMenu>
       <div className="ml-auto flex gap-2">
-        {user === null ? 
-        <Link href="/login" prefetch={false}><Button>로그인</Button></Link>
-        : <UserProfileDropDown {...user} />}
+        {user === null ? (
+          <Link href="/login" prefetch={false}>
+            <Button>로그인</Button>
+          </Link>
+        ) : (
+          <UserProfileDropDown {...user} />
+        )}
       </div>
     </header>
   );
