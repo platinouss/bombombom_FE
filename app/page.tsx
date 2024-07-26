29fcc331b4b76ae74cb41a571f5ec6f7cede8a0b
sseunghoon <sseunghun99@naver.com>
2024-07-26 20:43:47 +0900

    🧹Chore: Demo 용 메인화면 username 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 'use client';
 
+import { userState } from '@/recoil/userAtom';
+import { useRecoilState } from 'recoil';
+
 export default function Home() {
-  return <></>;
+  const [myData, setMyData] = useRecoilState(userState);
+  return (
+    <>
+      <div className="text-center">
+        {myData?.username + '로 로그인된 상태입니다.'}
+      </div>
+    </>
+  );
 }
