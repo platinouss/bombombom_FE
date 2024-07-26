c0ad3ef1a089b5ad131028d9ff53f72e08959e0e
sseunghoon <sseunghun99@naver.com>
2024-07-26 20:45:19 +0900

    ✨Feat: 새로고침하더라도 UserState 가 소멸되지 않도록 recoil-persist 추가

--- recoil/userAtom.ts
+++ recoil/userAtom.ts
@@ -1,7 +1,11 @@
-import { atom } from 'recoil';
 import { User } from '@/types/user/user';
+import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist();
 
 export const userState = atom<User | null>({
   key: 'userState',
-  default: null
+  default: null,
+  effects_UNSTABLE: [persistAtom]
 });
