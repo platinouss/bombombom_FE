9cbba5ae0dec7d83b11f42877f20912f5866e0cf
platinouss <platinouss@gmail.com>
2024-10-17 19:12:22 +0900

    [BBB-137] ✨Feat: 회원가입 진행 시 종단간 암호화 적용

--- lib/api/users/signup.ts
+++ lib/api/users/signup.ts
@@ -1,18 +1,33 @@
-import axios from 'axios';
-import { FieldValues } from 'react-hook-form';
+import axios, { AxiosError } from 'axios';
 
 /**
  * 회원가입 API
  * body : username, password, baekjoonId, introduce
  */
-export async function signup({
-  username,
-  password,
-  baekjoonId,
-  introduce
-}: FieldValues) {
-  return await axios.post(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/api/v1/users/signup`,
-    { username, password, baekjoonId, introduce }
-  );
+export async function signup(
+  id: number,
+  version: number,
+  encryptedData: string
+) {
+  try {
+    return await axios.post(
+      `${process.env.NEXT_PUBLIC_API_SERVER_URL}/api/v1/users/signup`,
+      {
+        id,
+        version,
+        encryptedData
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    if (axiosError.response?.status === 409) {
+      throw new Error('이미 존재하는 아이디입니다.');
+    }
+    throw new Error('회원가입에 실패했습니다.');
+  }
 }
